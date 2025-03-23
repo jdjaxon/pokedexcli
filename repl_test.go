198bb105b73e53db309a08e38ad6a5fcc97f5125
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-22 22:06:43 -0400

    Added unit tests for cleanInput.

---
+++ repl_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := map[string]struct {
+		input    string
+		expected []string
+	}{
+		"normal spacing": {
+			input:    "hello world",
+			expected: []string{"hello", "world"},
+		},
+		"extra outside spacing": {
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		"extra middle space": {
+			input:    "hello		world",
+			expected: []string{"hello", "world"},
+		},
+		"extra middle and outside space": {
+			input:    "    hello		world    ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for name, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("\nname: %v\n\texpected: %#v\n\tgot: %#v\n\n", name, c.expected, actual)
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("\nname: %v\n\texpected: %#v\n\tgot: %#v\n\n", name, expectedWord, word)
+			}
+		}
+	}
+}
