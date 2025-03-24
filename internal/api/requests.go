81c9df362b1bbaf4d6bec807c367e0f0d9ec2a28
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-24 18:59:07 -0400

    Implemented the cache checks inside the API request.

--- internal/api/requests.go
+++ internal/api/requests.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -11,20 +13,38 @@ func (c *Client) GetLocations(reqURL *string) (LocationResponse, error) {
 		url = *reqURL
 	}
 
+	var locResp LocationResponse
+	cachedResp, ok := c.reqCache.Get(url)
+	if ok {
+		err := json.Unmarshal(cachedResp, &locResp)
+		if err != nil {
+			return LocationResponse{}, err
+		}
+		return locResp, nil
+	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return LocationResponse{}, err
+		return locResp, err
 	}
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return LocationResponse{}, err
+		return locResp, err
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode > 299 {
+		return locResp, fmt.Errorf("failed with status code: %v", resp.StatusCode)
+	}
 
-	var locResp LocationResponse
-	decoder := json.NewDecoder(resp.Body)
-	err = decoder.Decode(&locResp)
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return locResp, err
+	}
+
+	c.reqCache.Add(url, data)
+
+	err = json.Unmarshal(data, &locResp)
 	if err != nil {
 		return LocationResponse{}, err
 	}
