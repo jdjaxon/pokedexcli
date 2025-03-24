555be408c086171cdb4e8193609984e6f5c5a0ed
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-24 18:56:08 -0400

    Added a constant for the client timeout.

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"github.com/jdjaxon/pokedexcli/internal/api"
 )
 
+const clientTimeout = 5 * time.Second
+
 func main() {
-	client := api.NewClient(10 * time.Second)
+	client := api.NewClient(clientTimeout)
 	conf := &config{
 		client: client,
 	}
