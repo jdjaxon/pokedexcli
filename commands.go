7b68cbcac9f097f50e29a081bacaa064dcf13f0e
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-24 14:29:27 -0400

    Renamed callbacks.go to commands.go. Command callbacks now rely on configuration passing in from main entrypoint. Config manages the client and next and previous URLs.

--- commands.go
+++ commands.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/jdjaxon/pokedexcli/internal/client"
 )
 
 // commandExit is the callback function to enable the user to exit the REPL.
-func commandExit() error {
+func commandExit(conf *config) error {
+
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
 // commandHelp is the callback function to enable the user to display the help.
-func commandHelp() error {
+func commandHelp(conf *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
@@ -29,20 +28,45 @@ func commandHelp() error {
 }
 
 // commandExit is the callback function to enable the user to exit the REPL.
-func commandMap() error {
-	areas, err := client.GetLocationAreas()
+func commandMap(conf *config) error {
+	if conf == nil {
+		return ErrBadConfig
+	}
+
+	resp, err := conf.client.GetLocations(conf.nextURL)
 	if err != nil {
 		return err
 	}
 
-	for _, area := range areas {
-		fmt.Println(area)
+	conf.nextURL = resp.Next
+	conf.previousURL = resp.Previous
+	for _, area := range resp.Results {
+		fmt.Println(area.Name)
 	}
 
 	return nil
 }
 
 // commandExit is the callback function to enable the user to exit the REPL.
-func commandMapBack() error {
+func commandMapBack(conf *config) error {
+	if conf == nil {
+		return ErrBadConfig
+	}
+
+	if conf.previousURL == nil {
+		return ErrFirstPage
+	}
+
+	resp, err := conf.client.GetLocations(conf.previousURL)
+	if err != nil {
+		return err
+	}
+
+	conf.nextURL = resp.Next
+	conf.previousURL = resp.Previous
+	for _, area := range resp.Results {
+		fmt.Println(area.Name)
+	}
+
 	return nil
 }
