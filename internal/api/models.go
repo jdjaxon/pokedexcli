3844fdbf2d9702c184754539f7dd803b4c43a048
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-25 14:58:45 -0400

    Added a data model struct for an explore command response.

--- internal/api/models.go
+++ internal/api/models.go
@@ -9,3 +9,18 @@ type LocationResponse struct {
 		URL  string `json:"url"`
 	} `json:"results"`
 }
+
+type ExploreResponse struct {
+	ID                   int    `json:"id"`
+	Name                 string `json:"name"`
+	Location struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+}
