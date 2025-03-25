721c94bf6318f9db6e4454251c091856059c954a
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-25 14:59:38 -0400

    Extracted similar request logic to a helper function and added a request for the explore command.

--- internal/api/requests.go
+++ internal/api/requests.go
@@ -7,47 +7,66 @@ import (
 	"net/http"
 )
 
+// GetLocations -
 func (c *Client) GetLocations(reqURL *string) (LocationResponse, error) {
-	url := baseURL + "location-area"
+	url := baseURL + locationEndpoint
 	if reqURL != nil {
 		url = *reqURL
 	}
 
 	var locResp LocationResponse
-	cachedResp, ok := c.reqCache.Get(url)
-	if ok {
-		err := json.Unmarshal(cachedResp, &locResp)
-		if err != nil {
-			return LocationResponse{}, err
-		}
-		return locResp, nil
-	}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return locResp, err
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return locResp, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode > 299 {
-		return locResp, fmt.Errorf("failed with status code: %v", resp.StatusCode)
-	}
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return locResp, err
-	}
-
-	c.reqCache.Add(url, data)
-
-	err = json.Unmarshal(data, &locResp)
+	err := c.getJSON(url, &locResp)
 	if err != nil {
 		return LocationResponse{}, err
 	}
 
 	return locResp, nil
+}
+
+// ExploreLocation -
+func (c *Client) ExploreLocation(location string) (ExploreResponse, error) {
+	if location == "" {
+		return ExploreResponse{}, ErrInvalidLoc
+	}
+
+	url := baseURL + locationEndpoint + location
+
+	var expResp ExploreResponse
+	err := c.getJSON(url, &expResp)
+	if err != nil {
+		return ExploreResponse{}, err
+	}
+
+	return expResp, nil
+}
+
+func (c *Client) getJSON(url string, target any) error {
+	cachedResp, ok := c.reqCache.Get(url)
+	if ok {
+		return json.Unmarshal(cachedResp, target)
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 299 {
+		return fmt.Errorf("failed with status code: %v", resp.StatusCode)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	c.reqCache.Add(url, data)
+
+	return json.Unmarshal(data, target)
 }
