538e27603dee304ee878b6a49a71891975b5176b
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-25 14:58:12 -0400

    Added a new error and a new location endpoint constant.

--- internal/api/api.go
+++ internal/api/api.go
@@ -5,9 +5,11 @@ import (
 )
 
 const (
-	baseURL = "https://pokeapi.co/api/v2/"
+	baseURL          = "https://pokeapi.co/api/v2/"
+	locationEndpoint = "location-area/"
 )
 
 var (
 	ErrInvalidUrl = errors.New("invalid or empty URL")
+	ErrInvalidLoc = errors.New("invalid or empty location")
 )
